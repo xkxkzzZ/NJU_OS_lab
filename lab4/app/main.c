e4b8c451bffe518b9d2c71becba69e3ee1ec5779
xkxkzzZ <221220074@smail.nju.edu.cn>
2024-05-14 10:45:21 +0800

    lab4-original

---
+++ lab4/app/main.c
@@ -0,0 +1,70 @@
+#include "lib.h"
+#include "types.h"
+
+int uEntry(void)
+{
+	// For lab4.1
+	// Test 'scanf'
+	int dec = 0;
+	int hex = 0;
+	char str[6];
+	char cha = 0;
+	int ret = 0;
+	while (1)
+	{
+		printf("Input:\" Test %%c Test %%6s %%d %%x\"\n");
+		ret = scanf(" Test %c Test %6s %d %x", &cha, str, &dec, &hex);
+		printf("Ret: %d; %c, %s, %d, %x.\n", ret, cha, str, dec, hex);
+		if (ret == 4)
+			break;
+	}
+
+	// For lab4.2
+	// Test 'Semaphore'
+	int i = 4;
+
+	sem_t sem;
+	printf("Father Process: Semaphore Initializing.\n");
+	ret = sem_init(&sem, 2);
+	if (ret == -1)
+	{
+		printf("Father Process: Semaphore Initializing Failed.\n");
+		exit();
+	}
+
+	ret = fork();
+	if (ret == 0)
+	{
+		while (i != 0)
+		{
+			i--;
+			printf("Child Process: Semaphore Waiting.\n");
+			sem_wait(&sem);
+			printf("Child Process: In Critical Area.\n");
+		}
+		printf("Child Process: Semaphore Destroying.\n");
+		sem_destroy(&sem);
+		exit();
+	}
+	else if (ret != -1)
+	{
+		while (i != 0)
+		{
+			i--;
+			printf("Father Process: Sleeping.\n");
+			sleep(128);
+			printf("Father Process: Semaphore Posting.\n");
+			sem_post(&sem);
+		}
+		printf("Father Process: Semaphore Destroying.\n");
+		sem_destroy(&sem);
+		exit();
+	}
+
+	// For lab4.3
+	// TODO: You need to design and test the problem.
+	// Note that you can create your own functions.
+	// Requirements are demonstrated in the guide.
+
+	return 0;
+}
